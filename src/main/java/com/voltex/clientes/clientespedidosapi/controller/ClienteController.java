8320c57eecfb47f30a7e1fd4aa8edd1658e44be2
Fran Jimenez <fjimenezjob@gmail.com>
2021-05-08 20:18:52 +0200

    Minimal Corrections

--- src/main/java/com/voltex/clientes/clientespedidosapi/controller/ClienteController.java
+++ src/main/java/com/voltex/clientes/clientespedidosapi/controller/ClienteController.java
@@ -7,7 +7,6 @@ import com.voltex.clientes.clientespedidosapi.service.ClienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
